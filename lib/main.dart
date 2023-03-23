2069f21c0ec0f562b94849797e5ffca1142a5c39
drewkubacki <111913572+drewkubacki@users.noreply.github.com>
2023-03-22 20:20:34 -0400

    Added addError to the increment method and OnError to observe the error globally from the observer

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,22 @@ Future<void> main() async {
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
-  void increment() => emit(state + 1);
+  void increment() {
+    addError(Exception('increment error!'), StackTrace.current);
+    emit(state + 1);
+  }
 
   @override
   void onChange(Change<int> change) {
     super.onChange(change);
     print(change);
   }
+
+  /*  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error $stackTrace');
+    super.onError(error, stackTrace);
+  } */
 }
 
 class SimpleBlocObserver extends BlocObserver {
@@ -30,5 +39,11 @@ class SimpleBlocObserver extends BlocObserver {
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
     print('${bloc.runtimeType} $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
   }
 }
