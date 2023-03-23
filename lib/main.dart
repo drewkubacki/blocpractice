6c6ea9ede0c43d492a000ae3987f4696fcf49690
drewkubacki <111913572+drewkubacki@users.noreply.github.com>
2023-03-22 21:50:46 -0400

    Practice handling events with Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
-  Bloc.observer = SimpleBlocObserver();
-  final cubit = CounterCubit();
-  final subscription = cubit.stream.listen(print);
-  cubit.increment();
+  final bloc = CounterBloc();
+  print(bloc.state);
+  bloc.add(CounterIncrementPressed());
   await Future.delayed(Duration.zero);
-  await subscription.cancel();
-  await cubit.close();
+  print(bloc.state);
+  await bloc.close();
 }
 
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
+abstract class CounterEvent {}
 
-  void increment() {
-    addError(Exception('increment error!'), StackTrace.current);
-    emit(state + 1);
-  }
+class CounterIncrementPressed extends CounterEvent {}
 
-  @override
-  void onChange(Change<int> change) {
-    super.onChange(change);
-    print(change);
-  }
-
-  /*  @override
-  void onError(Object error, StackTrace stackTrace) {
-    print('$error $stackTrace');
-    super.onError(error, stackTrace);
-  } */
-}
-
-class SimpleBlocObserver extends BlocObserver {
-  @override
-  void onChange(BlocBase bloc, Change change) {
-    super.onChange(bloc, change);
-    print('${bloc.runtimeType} $change');
-  }
-
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    print('${bloc.runtimeType} $error $stackTrace');
-    super.onError(bloc, error, stackTrace);
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) {
+      emit(state + 1);
+    });
   }
 }
