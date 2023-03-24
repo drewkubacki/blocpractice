9da15d93d66740be0009f8814e390f56ea1f31b9
drewkubacki <111913572+drewkubacki@users.noreply.github.com>
2023-03-23 22:12:19 -0400

    Working with BlocObserver to handle changes and transitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
+  Bloc.observer = SimpleBlocObserver();
+  CounterBloc();
   final bloc = CounterBloc();
-  print(bloc.state);
-  final subscription = bloc.stream.listen(print);
+  //final subscription = bloc.stream.listen(print);
   bloc.add(CounterIncrementPressed());
   await Future.delayed(Duration.zero);
-  await subscription.cancel();
+  //await subscription.cancel();
   await bloc.close();
 }
 
@@ -22,5 +22,37 @@ class CounterBloc extends Bloc<CounterEvent, int> {
     on<CounterIncrementPressed>((event, emit) {
       emit(state + 1);
     });
+  }
+
+/*   @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  } */
+
+/*   @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    super.onTransition(transition);
+    print(transition);
+  } */
+}
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print('{$bloc.runtimeType} $transition');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
   }
 }
