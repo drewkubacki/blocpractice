398e91d61eeb160be1475d70b92e25a9ac479325
drewkubacki <111913572+drewkubacki@users.noreply.github.com>
2023-03-26 21:26:26 -0400

    Utilizing BlocProvider, BlocBuilder and BlocListener for a simple counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,93 @@
-import 'dart:html';
+import 'package:blocpractice/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-Future<void> main() async {
-  Bloc.observer = SimpleBlocObserver();
-  CounterBloc();
-  final bloc = CounterBloc();
-  //final subscription = bloc.stream.listen(print);
-  bloc.add(CounterIncrementPressed());
-  await Future.delayed(Duration.zero);
-  //await subscription.cancel();
-  await bloc.close();
+void main() {
+  runApp(const MyApp());
 }
 
-abstract class CounterEvent {}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class CounterIncrementPressed extends CounterEvent {}
-
-class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc() : super(0) {
-    on<CounterIncrementPressed>((event, emit) {
-      emit(state + 1);
-    });
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
   }
-
-/*   @override
-  void onChange(Change<int> change) {
-    super.onChange(change);
-    print(change);
-  } */
-
-/*   @override
-  void onTransition(Transition<CounterEvent, int> transition) {
-    super.onTransition(transition);
-    print(transition);
-  } */
 }
 
-class SimpleBlocObserver extends BlocObserver {
-  @override
-  void onChange(BlocBase bloc, Change change) {
-    super.onChange(bloc, change);
-    print('${bloc.runtimeType} $change');
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    print('{$bloc.runtimeType} $transition');
-  }
-
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    print('${bloc.runtimeType} $error $stackTrace');
-    super.onError(bloc, error, stackTrace);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Counter"),
+      ),
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.isIncremented == true) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                duration: Duration(milliseconds: 300),
+                content: Text('Count Was Incremented!')));
+          } else if (state.isIncremented == false) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                duration: Duration(milliseconds: 300),
+                content: Text('Count Was Decremented!')));
+          }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Current Count"),
+              BlocBuilder<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text(state.counterValue.toString(),
+                      style: const TextStyle(fontSize: 32));
+                },
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterCubit>().increment();
+                    },
+                    tooltip: 'Increment',
+                    heroTag: "fab1",
+                    child: const Icon(Icons.add),
+                  ),
+                  const SizedBox(width: 30),
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterCubit>().decrement();
+                    },
+                    tooltip: 'Increment',
+                    heroTag: "fab2",
+                    child: const Icon(Icons.remove),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
